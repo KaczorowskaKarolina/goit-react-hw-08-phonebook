3a85377853d770a715b3ec74d1766e7025759b3a
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-12-10 22:55:20 +0100

    depcheck

--- src/components/Navigation/UserMenu.jsx
+++ src/components/Navigation/UserMenu.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const UserMenu = ({ userEmail, onLogout }) => {
   return (
     <div>
+      <NavLink exact to="/register" activeClassName="active">
+        Register
+      </NavLink>
+      <NavLink exact to="/login" activeClassName="active">
+        Login
+      </NavLink>
       <p>{userEmail}</p>
       <button onClick={onLogout}>Logout</button>
     </div>
